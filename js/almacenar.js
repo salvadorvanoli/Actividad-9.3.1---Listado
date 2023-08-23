d57b02b658b09515b414787621feafd20aead2d5
salvadorvanoli <141060855+salvadorvanoli@users.noreply.github.com>
2023-08-23 19:39:54 -0300

    Solución mejorada

--- js/almacenar.js
+++ js/almacenar.js
@@ -2,11 +2,10 @@ document.addEventListener("DOMContentLoaded", function(){
     let agregar = document.getElementById("agregar");
     let item = document.getElementById("item");
     let limpiar = document.getElementById("limpiar");
-    let datos = [localStorage.getItem("array")];
+    let datos = JSON.parse(localStorage.getItem("array"));
     if (datos) {
         escribirLista(datos);
     }
-    
 
     function escribirLista(datos) {
         let texto = "";
@@ -28,10 +27,10 @@ document.addEventListener("DOMContentLoaded", function(){
     agregar.addEventListener("click", function(){
         if(item.value){
             datos.push(item.value);
-            localStorage.setItem("array", datos);
+            localStorage.setItem("array", JSON.stringify(datos));
             escribirLista(datos);
+            item.value = "";
         }
-        item.value = "";
     });
 
     limpiar.addEventListener("click", function(){
