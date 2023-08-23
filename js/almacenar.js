8ff2b82083f360ab197bfa6cf80cd719356e052b
salvadorvanoli <141060855+salvadorvanoli@users.noreply.github.com>
2023-08-23 19:20:37 -0300

    Primera solución

---
+++ js/almacenar.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let agregar = document.getElementById("agregar");
+    let item = document.getElementById("item");
+    let limpiar = document.getElementById("limpiar");
+    let datos = [localStorage.getItem("array")];
+    if (datos) {
+        escribirLista(datos);
+    }
+    
+
+    function escribirLista(datos) {
+        let texto = "";
+        datos.forEach(elm => {
+            texto += 
+            `<ul> 
+            <li>${elm}</li>
+            </ul>
+            `
+            document.getElementById("contenedor").innerHTML = texto;
+        })
+    }
+
+    function borrarContenedor(array){
+        document.getElementById("contenedor").innerHTML = "";
+        array.splice(0);
+    }
+
+    agregar.addEventListener("click", function(){
+        if(item.value){
+            datos.push(item.value);
+            localStorage.setItem("array", datos);
+            escribirLista(datos);
+        }
+        item.value = "";
+    });
+
+    limpiar.addEventListener("click", function(){
+        localStorage.clear();
+        borrarContenedor(datos);
+    });
+});
